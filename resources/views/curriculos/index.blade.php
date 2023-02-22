749805876b05cac919f6fd565d8f736aad906997
herculesoliveira40 <herculesoliveira40@gmail.com>
2023-02-22 16:14:19 -0300

    button search

--- resources/views/curriculos/index.blade.php
+++ resources/views/curriculos/index.blade.php
@@ -3,7 +3,13 @@
 
 @section('content')
     <h1> Curriculos <a href="{{ route('curriculos.create') }}"> +</a></h1>
-
+    <div>
+        <form action="{{ route('curriculos.index')}}" method="get"> 
+            <input type="text" name="search" placeholder="Pesquisar Nome ou ID" >
+            <button class="btn btn-outline-primary" > Pesquisar </button>
+            <h3 class="text-warning">Buscar por: {{request()->input('search')}}</h3>
+        </form>
+    </div>
     @include('curriculos._partials.table')
 
 
